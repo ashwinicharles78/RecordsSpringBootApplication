07ea93042a0dc2a5f8f4720019d3a0815b79b595
Ashwini Charles <ashwinicharles78@gmail.com>
2024-03-13 19:08:28 +0530

    Added health check endpoint again

--- src/main/java/com/records/Records/Controller/RecordsController.java
+++ src/main/java/com/records/Records/Controller/RecordsController.java
@@ -44,4 +44,9 @@ public class RecordsController {
         return service.deleteRecord(new Mykey(mykey));
     }
 
+    @GetMapping("/")
+    public String testHealth() {
+        return "tested";
+    }
+
 }

1a3443bcfacb5cfb5f2c245a09a87043c79fa394
Ashwini Charles <ashwini.charles@enterprisewide.com>
2024-04-03 16:43:23 +0530

    added topic name to environment

--- src/main/java/com/records/Records/config/KafkaProducerConfig.java
+++ src/main/java/com/records/Records/config/KafkaProducerConfig.java
@@ -2,8 +2,10 @@ package com.records.Records.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -16,9 +18,14 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Autowired
+    private Environment env;
+
+    private static final String TOPIC_NAME = "kafka.topic.name";
+
     @Bean
     public NewTopic createTopic(){
-        return new NewTopic("test", 3, (short) 1);
+        return new NewTopic(env.getProperty(TOPIC_NAME), 1, (short) 1);
     }
 
     @Bean
